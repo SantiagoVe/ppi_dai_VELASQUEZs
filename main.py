544d6271e255caa2618acfa2f6eeeb41cad294b7
Santiago Velásquez Jaramillo <55595196+SantiagoVe@users.noreply.github.com>
2024-04-15 01:11:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-import about, account, home, houses
+import about, account, home, post
 
+# Configuración de la página
 st.set_page_config(
     page_title="Busca tu casa",
 )
@@ -11,17 +12,28 @@ class MultiApp:
     def __init__(self):
         self.apps = []
     def add_app(self, title, function):
+        """
+        Agrega una aplicación a la lista de aplicaciones disponibles.
+
+        Args:
+            title (str): Título de la aplicación.
+            function (function): Función que ejecuta la aplicación.
+        """
         self.apps.append({
         "title": title,
         "function":  function
     })
 
     def run():
+        """
+        Ejecuta la aplicación multiaplicación.
+        """
 
         with st.sidebar:
+            # Menú de opciones para seleccionar la aplicación
             app = option_menu(
                 menu_title='Busca tu casa ',
-                options=['Home','Account','Houses','about'],
+                options=['Inicio','Cuenta','Tus publicaciones','Acerca de'],
                 icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
                 menu_icon='chat-text-fill',
                 default_index=1,
@@ -33,16 +45,16 @@ class MultiApp:
                 
                 )
 
-        
-        if app == "Home":
+        # Ejecutar la aplicación correspondiente seleccionada en el menú
+        if app == "Inicio":
             home.app()
-        if app == "Account":
+        if app == "Cuenta":
             account.app()    
-        if app == "Houses":
-            houses.app()        
-        if app == 'about':
+        if app == "Tus publicaciones":
+            post.app()        
+        if app == 'Acerca de':
             about.app()    
              
           
-             
+     # Crear una instancia de la clase MultiApp y ejecutar la aplicación        
     run()
