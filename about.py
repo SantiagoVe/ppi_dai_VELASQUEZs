eefff754f720b7ad266d923255a3bd7aef3b73cc
Santiago Velásquez Jaramillo <55595196+SantiagoVe@users.noreply.github.com>
2024-04-15 00:57:55 -0500

    Actualización about.py

---
+++ about.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def app():
+    """
+    Función que muestra información sobre el sitio web Busca tu casa.
+    """
+    st.subheader('Busca tu casa es un sitio web creado para que todo el mundo tenga oportunidad de encontrar su nuevo hogar.')
+    st.markdown('Creado por: [Santiago Velásquez Jaramillo](https://github.com/SantiagoVe)')
+    st.markdown('Contacto por correo electrónico: [svelasquezj@unal.edu.co]')
